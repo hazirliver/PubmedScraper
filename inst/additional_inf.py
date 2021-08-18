9d0cbf3500975f2e4dffd3302654f8af8c934890
hazirliver <cs-mitino@mail.ru>
2021-08-18 21:51:01 +0300

    upd

--- inst/additional_inf.py
+++ inst/additional_inf.py
@@ -1,12 +1,14 @@
 import argparse
 import pandas as pd
-from Bio import Entrez
-from Bio import Medline
+from bio import Entrez
+from bio import Medline
 
 
 def main_foo(pmids_lst):
     handle = Entrez.efetch(db='pubmed', id=pmids_lst, rettype='medline', retmode='text')
     records = Medline.parse(handle)
+
+    
 
     df = pd.DataFrame(records)
     return(df)
@@ -15,5 +17,6 @@ def main_foo(pmids_lst):
 
 
 if __name__ == '__main__':
+    print(help("modules"))
     Entrez.email = "Your.Name.Here@example.org"
 
