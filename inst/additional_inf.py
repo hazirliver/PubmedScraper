39890b178ef83387917a55ee12547ea8bc486aca
hazirliver <cs-mitino@mail.ru>
2021-08-18 21:54:06 +0300

    upd

--- inst/additional_inf.py
+++ inst/additional_inf.py
@@ -1,17 +1,17 @@
-import argparse
-import pandas as pd
-from bio import Entrez
-from bio import Medline
+#import argparse
+#import pandas as pd
+#from bio import Entrez
+#from bio import Medline
 
 
 def main_foo(pmids_lst):
-    handle = Entrez.efetch(db='pubmed', id=pmids_lst, rettype='medline', retmode='text')
-    records = Medline.parse(handle)
-
+    #handle = Entrez.efetch(db='pubmed', id=pmids_lst, rettype='medline', retmode='text')
+    #records = Medline.parse(handle)
     
+    print(help("modules"))
 
-    df = pd.DataFrame(records)
-    return(df)
+    #df = pd.DataFrame(records)
+    #return(df)
 
 
 
